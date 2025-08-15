76c567a5c12f2aa391b7356b1579e2bf88aa452c
Zytronium <dastelljes@outlook.com>
2025-08-15 16:42:02 -0500

    Added comments to explain the code better

--- passwordGuesser.py
+++ passwordGuesser.py
@@ -19,7 +19,10 @@ def print_nonsense():
     user's password based on the given credentials. This contains several
     jokes as well.
     """
+
+    # Generate a random number the size of the list below
     rand = randint(0, 26)
+    # List of random messages to print
     print_msgs = {
         0: "Hashing provided username...",
         1: f"Calculating fastest method to decrypt password for user "
@@ -54,23 +57,33 @@ def print_nonsense():
         26: "Evaluating plan for world domin-\nPlease ignore the previous "
             "message.\nGenerating fake replacement message..."
     }
+    # Print a random message from the list above
     print(print_msgs[rand])
 
 
 if __name__ == '__main__':
+    # Welcome message
     print("Welcome to the Password Guesser! To use our services, please login "
           "or create an account.")
     sleep(0.3333)
+
+    # Prompt user for a username and password
     print("Please enter a username.")
     print("Username:", end=' ')
     username = input()
+
     print("Please enter a password for {name}.".format(name=username))
     print("Password:", end=' ')
     password = input()
+
     pass  # word
+
+    # Pretend to think for a few seconds and appear to be doing something
     print("Thinking...\n")
     sleep(0.5)
     for _ in range(0, 25):
         print_nonsense()
-        sleep(randint(30, 500)*0.001)
+        sleep(randint(30, 500)*0.001)  # 30 - 500 milliseconds
+
+    # Print out the password "guessed" (the password given at the start)
     print("\nYour password is: '{pw}'".format(pw=password))
